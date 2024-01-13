9b3a40bcddcc75a021edca7e8bfb652f64cf923c
--replace-all <dols3m@gmail.com>
2024-01-13 12:30:20 -0500

    initial

---
+++ entrypoint.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+HELP = ''
+
+if not 'cert' in os.environ:
+  print('Error: missing "cert" environment variable. ' + HELP, file=sys.stderr)
+  exit(1)
+if not 'key' in os.environ:
+  print('Error: missing "key" environment variable. ' + HELP, file=sys.stderr)
+  exit(1)
+
+with open('/cert.pem', 'w') as f:
+  f.write(os.environ['cert'])
+with open('/key.pem', 'w') as f:
+  f.write(os.environ['key'])
+
+os.execvp('cqlsh', ['cqlsh'] + sys.argv[1:])
